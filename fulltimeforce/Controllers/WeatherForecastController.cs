1a0f7ab86d05e327b4716da138fa7e7c88e16032
Andres Granja <andresgranja001@gmail.com>
2021-11-09 21:31:57 -0500

    new branch

--- fulltimeforce/Controllers/WeatherForecastController.cs
+++ fulltimeforce/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace fulltimeforce.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
